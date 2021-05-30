5df2f7d4dc9418b0523521195dbfd83945a28dc4
santoscigor <santoscigor@gmail.com>
2021-05-29 23:55:28 -0300

    feat: update app,js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
-const express = require('express')
+import express from "express"
+import bodyParser from "body-parser"
+import routes from "./routes";
+import Knex from "knex";
+import knexConfig from "../knexfile.js"
+import { Model } from "objection";
+
+require('dotenv'). config();
 
 const app = express()
 
-app.get('*', (req,res) => {
-  res.send('Hello World!')
-})
+const knex = Knex(knexConfig.development);
+Model.knex(knex);
 
-app.listen(process.env.PORT || '8080')
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
+app.use(routes);
+
+// request to handle undefined or all other routes
+app.get("*", function(res) {
+    res.send("App online and working!");
+ })
+
+app.listen(process.env.PORT || '8080', () => { console.log("running server on from port:::::::" + process.env.PORT?? '8080') })
+
+export default app;
