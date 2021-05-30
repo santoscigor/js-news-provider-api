e16278456f1d5f16f9e96dd8821f36978a456d71
santoscigor <santoscigor@gmail.com>
2021-05-29 23:50:43 -0300

    feat: admin and auth middlewares

---
+++ src/middlewares/admin.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+export function admin(req, res, next) {
+  const token = req.headers["x-access-token"];
+
+  if(!token){
+    return res.status(401).send({ error: "You must provide a token." });
+  }
+
+  try {
+    const decoded = jwt.verify(token,  process.env.JWT_SECRET);
+
+    if (typeof decoded !== "string") {
+      req.user = decoded;
+    }
+
+    if (req.user?.isAdmin !== true) {
+      return res.status(401).send({ error: "You do not have permission." });
+    }
+
+    return next();
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send({ error: "Ops! There were an error trying to validate your permission." });
+  }
+}
+
+export default admin;
---
+++ src/middlewares/auth.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import Token from "../models/token";
+
+export async function auth(req, res, next) {
+  const token = req.headers["x-access-token"];
+
+  try {
+    const validToken = token
+
+    if(!validToken){
+      req.user = null;
+    } else {
+      const tokenInDb = await Token.query().findOne({ token: validToken });
+      
+      if(tokenInDb){
+        const decoded = jwt.verify(validToken, process.env.JWT_SECRET);
+        req.user = decoded;
+      }
+    }
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ error: `errorMessages.Fatal.auth.login` });
+  }
+}
+
+export default auth;
